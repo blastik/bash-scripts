bc31d57a59f29a10cb54dec9c261317827d69bfe
David <blastikman@gmail.com>
2017-06-14 09:59:09 +0200

    Add files via upload

---
+++ borrarlogs.sh
@@ -0,0 +1,14 @@
+﻿#!/bin/sh
+
+maquinas=maquinascampus.txt
+dirs=`cat dirscampus.txt`
+
+while read -r server; do
+echo "$server"
+ssh -T root@"$server" << EOSSH
+    for i in "$dirs"; do
+    echo "eliminando logs de más de 1 año en \$i"
+	find \$i -type f \( -name \*.log -o -name \*.gz \)  -mtime +365 -exec rm {} \;
+    done
+EOSSH
+done < $maquinas
