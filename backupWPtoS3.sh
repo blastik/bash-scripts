48e6bd8e9f7175957fbf523fffbc95bbc0e79713
david.calvo <david.calvo@scmspain.com>
2018-02-19 12:15:07 +0100

    enable several dirs backup

--- backupWPtoS3.sh
+++ backupWPtoS3.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 
-#quick and dirty script to full backup wordpress to S3 and send SNS alert
+#quick and dirty script to full backup wordpress and other dirs to S3 and send SNS alert
 
 timestamp=`date '+%d%m%Y'`
-sourcedir=/var/www/
+sourcedirs="/path/2/backup;/path2/2/backup;/path3/2/backup"
 destdir=/tmp
-dbname="value"
-dbuser="value"
-dbpass="value"
-S3bucket="value"
-SNSarn="value"
+dbname="dbname"
+dbuser="dbuser"
+dbpass="dbpass"
+S3bucket="S3bucket"
+SNSarn="SNSarn"
 
-echo "compressing $sourcedir to $destdir/wordpress-backup-$timestamp.zip"
-cd $sourcedir
-zip -r $destdir/wordpress-backup-$timestamp.zip *
+export IFS=";"
+for i in $sourcedirs; do
+  echo 'compressing '$sourcedir' to '$destdir'/'${i///}'-'$timestamp'.zip'
+  cd $i
+  zip -r $destdir/${i///}-$timestamp.zip *
+done
 
 echo "dumping $dbname sql database"
 mysqldump -u$dbuser -p$dbpass $dbname > $destdir/database.sql
 
-echo "appending sql database to wordpress-backup-$timestamp.zip"
+echo "appending sql database to varwww-$timestamp.zip"
 cd $destdir
-zip -um wordpress-backup-$timestamp.zip database.sql
+zip -um varwww-$timestamp.zip database.sql
 
-echo "uploading files to S3"
-move2s3=$(/usr/local/bin/aws s3 mv $destdir/wordpress-backup-$timestamp.zip s3://$S3bucket 2>&1)
+for i in $sourcedirs; do
+  echo 'uploading '${i///}'-'$timestamp'.zip to S3'
+  move2s3=$(aws s3 mv $destdir/${i///}-$timestamp.zip s3://$S3bucket 2>&1)
+done
+
 if [ $? != 0 ]; then
-    /usr/local/bin/aws sns publish --topic-arn $SNSarn --subject "Wordpress backup FAILED" --message "$move2s3"
+  aws sns publish --topic-arn $SNSarn --subject "AWS personal backup FAILED" --message "$move2s3"
 else
-    /usr/local/bin/aws sns publish --topic-arn $SNSarn --subject "Wordpress backup DONE" --message "$move2s3"
+  aws sns publish --topic-arn $SNSarn --subject "AWS personal backup DONE" --message "$move2s3"
 fi
