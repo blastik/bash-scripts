3a629408762730b39f10a859fb7b389e4b07b845
david.calvo <david.calvo@scmspain.com>
2018-02-19 20:56:38 +0100

    change ls-remote

--- CheckStaledBranches.sh
+++ CheckStaledBranches.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e -u
-git ls-remote origin 'refs/heads/*' | while read sha ref; do
+git ls-remote -h 'https://github.com/blastik/bash-scripts' | while read sha ref; do
   # test if $sha is merged
   E=`git cat-file -t "$sha" 2>&1`
   test $? -ne 0 -a "${E#*git cat-file: *}" = "could not get object info" && continue
